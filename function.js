5bad37e82b5d7a442ede8630ceb30b6bc6c0e44c
shrikant <skanthale9@gmail.com>
2021-03-20 02:31:33 -0700

    discuss about function in js

---
+++ function.js
@@ -0,0 +1,44 @@
+/*alph()
+numeric()
+//alph()
+//numeric()
+
+function alph(){
+    console.log("S")
+    console.log("H")
+    console.log("R")
+    console.log("I")
+}
+
+function numeric(){
+    console.log("1")
+    console.log("9")
+    console.log("9")
+    console.log("8")
+}
+*/
+/*
+function add(a,b){
+    var result = a+b
+    console.log(result)
+}
+
+function mul(m,n){
+    var res1 = n * m;
+    console.log(res1)
+} 
+ 
+function sub(j,k){
+    var res2 = j-k
+    console.log(res2)
+} 
+
+function div(x,y){
+    var res3 = x/y;
+    console.log(res3)
+} 
+add(10,15)
+mul(20 , 10)
+sub(45,54)
+div(25,5)
+*/

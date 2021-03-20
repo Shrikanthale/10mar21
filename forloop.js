c9eecbfdff9a4c93b7aba5d35a0bf2d76dd2f4e0
shrikant <skanthale9@gmail.com>
2021-03-20 01:59:06 -0700

    discuss about for loop in javascript

---
+++ forloop.js
@@ -0,0 +1,31 @@
+/*var i =1 
+for(i=1 ; i<=10 ; i++){
+    console.log(i);
+}
+*/
+/*
+var i =1 
+for(i=1 ; i<=10 ; i++){
+    if(i%2==0){
+    console.log(i);
+}
+}
+*/
+
+/*
+var i = 1
+var num = 15 
+for(i=1 ; i<=10 ; i++){
+    var table
+    table = num * i ;
+    console.log(table)
+}
+*/
+
+/* 
+for(var i=1 ; i<=100 ; i++){
+    if(i%4==0 && i%7==0){
+        console.log(i);
+    } 
+}
+*/

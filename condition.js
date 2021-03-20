b34c3976847825acdee54be21d6d9fc27cc76284
shrikant <skanthale9@gmail.com>
2021-03-19 23:41:49 -0700

    discuss about if else conditions in js

---
+++ condition.js
@@ -0,0 +1,30 @@
+/*var num = 20 
+if(num%2==0){
+    console.log("even");
+}
+num = 21
+if(num%2!=0){
+    console.log("odd");
+}
+*/
+/*
+var number = -12 
+if(number>=0){
+    console.log("positive number");
+}
+else {
+    console.log("negative number");
+}
+*/
+
+var num1 = 10 
+var num2 = 20 
+var num3 = 30
+
+if(num1>=num2 && num1>=num3){
+    console.log("num1 is greater than num2 and num3")
+} else if(num2>=num1 && num2>=num3){
+    console.log("num2 is greater than num1 and num3")
+}else{
+    console.log("num3 is grater than num1 and num2")
+}

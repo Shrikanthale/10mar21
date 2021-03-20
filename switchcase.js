5d34d304427484f6ec7c4848451522156647a07e
shrikant <skanthale9@gmail.com>
2021-03-20 02:14:07 -0700

    discuss about switch case in js

---
+++ switchcase.js
@@ -0,0 +1,42 @@
+// switch case 
+/*var time = 12
+
+switch(time){
+    case 0:
+        console.log("good morning");
+        break;
+    
+    case 12 :
+        console.log("good afternoon");
+        break;
+        
+    case 6 : 
+         console.log("good evening");
+         break;
+         
+    default:
+        console.log("good night");      
+
+}
+*/
+
+
+var temp = 30
+
+switch(temp){
+    case 10 :
+        console.log("COLD");
+        break;
+
+    case 20 :
+        console.log("COLD AND HOT");
+        break;
+       
+    case 30 : 
+        console.log("HOT");
+        break;
+        
+     default : 
+        console.log("ERROR!!!");    
+
+}

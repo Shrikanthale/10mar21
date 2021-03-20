57c4da3a333a8f73cc66f28e0357d18d5d1d9276
shrikant <skanthale9@gmail.com>
2021-03-19 23:25:49 -0700

    discuss about basic javascript

---
+++ basicjs.js
@@ -0,0 +1,17 @@
+ /*people = {name: "sham", height: 6.1, weight: 57, color: "brown"}
+ console.log(people)
+ console.log(people.name);
+ console.log(people.height);
+ console.log(people.weight);
+ */
+/*
+ var istrue = "sham"
+ istrue = true;
+ console.log(istrue);
+ */
+/*
+var num = 10+70+"sham"
+console.log(num)
+num = "sham"+10+70
+console.log(num)
+*/

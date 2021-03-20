78a197d627b8ce389450cda170f90d81a7e74d04
shrikant <skanthale9@gmail.com>
2021-03-20 00:22:33 -0700

    discuss about loops like while and do while loops in javascript

---
+++ loops.js
@@ -0,0 +1,61 @@
+/*var num = 5
+while(num>=0){
+    console.log("shrikant")
+    num--
+} 
+*/ 
+/*
+var num = 5
+while(num>0){
+    console.log("shrikant")
+    num--
+    console.log("remining number of count" , num)
+} 
+*/
+/*
+var i = 1 
+while(i<=10){
+    var mul = 5
+    mul=mul*i;
+    console.log(mul)
+    i++
+}
+*/
+/*
+var num = 1
+while(num<=10){
+    if(num%2==0){
+        var temp = null ;
+        temp=temp + num 
+        console.log(temp)
+        console.log(num)
+    }
+    num++
+}
+*/
+/*
+var i = 1 
+while(i<=100){
+    if(i%2==0){
+      //  if(i%4==0){
+    console.log(i)
+    }
+//}
+    i++
+}
+*/
+/*
+var i = 1 
+do{
+    console.log(i)
+     i++
+}while(i<=10)
+*/
+
+var i = 1 
+do {
+    var num = 15
+    num = num * i 
+    console.log(num)
+    i++
+}while(i<=10)

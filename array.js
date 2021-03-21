e30aaef71832450fa41570e6158f649b6803f8ae
shrikant <skanthale9@gmail.com>
2021-03-21 03:26:23 -0700

    discuss about array in js

---
+++ array.js
@@ -0,0 +1,27 @@
+/*var name=["sham","ram","akash","nirmal"]
+
+console.log(name[2])
+console.log(name[0])
+console.log(name[3])
+name.push("shewata")
+console.log(name)
+console.log(name.length)
+console.log(name[name.length-1])
+name.pop("ram")
+console.log(name)
+console.log(name.length)
+console.log(name[name.length-2])
+*/
+
+var number = [45,56,32,18,20]
+//console.log(number[2])
+//console.log(number.length)
+//number.push(30)
+//console.log(number)
+//console.log(number[number.length-3])
+//console.log(number[0]+number[1]+number[2]+number[3]+number[4])
+//console.log(number.sort())
+//Math.max.apply(number)
+console.log(number.Math.max())
+
+//var a = Math.max(45,56,32,18,20)

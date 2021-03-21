d457f1002c2f1d20f1c3831b98c0f55aa1ef9045
shrikant <skanthale9@gmail.com>
2021-03-21 02:04:38 -0700

    discuss objfun acess

---
+++ objfun.js
@@ -0,0 +1,50 @@
+/*var student = {
+    Name:"shri",
+    ID:86,
+    add:"@ airport",
+    gender:"M",
+    
+    Deatils : function(){
+        console.log(this.ID)
+        console.log(this.Name)
+        console.log(this.add)
+        console.log(this.gender)
+    }
+}
+student.Deatils()
+*/
+/*
+var people = {
+    Name : "sham",
+    course:"cs",
+    codeno:1452,
+    color:"black",
+
+    Deatils : function(){
+        console.log(this.Name)
+        console.log(this.codeno)
+        console.log(this.course)
+        console.log(this.color)
+    }
+}
+people.Deatils()
+*/
+
+var people = {
+    Name : "sham",
+    course:"cs",
+    codeno:1452,
+    color:"black",
+
+    Deatils : function(){
+        console.log(this.Name)
+        console.log(this.codeno)
+        console.log(this.course)
+        console.log(this.color)
+    },
+
+    getName : function(LastName){
+        return this.Name+" "+LastName
+    }
+}
+console.log(people.getName("sharma"))
